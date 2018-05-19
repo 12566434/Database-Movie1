af4500b95568d32b5b1d1a863bb9b2d98066832e
12566434 <37357655+12566434@users.noreply.github.com>
2018-05-19 16:05:50 +1000

    Update Movie.java

--- app/src/main/java/com/example/zain/myapplication/Movie.java
+++ app/src/main/java/com/example/zain/myapplication/Movie.java
@@ -1,21 +1,19 @@
-package com.example.zain.myapplication;
-
 
 public class Movie {
     private int ID;
     private String name;
-    private double price1;
+    /*private double price1;
     private double price2;
-    private double price3;
+    private double price3;*/
     private String movieDescription;
     private int movieRating;
 
-    public Movie(int ID, String name, double price1, double price2, double price3, String movieDescription, int movieRating){
+    public Movie(int ID, String name, double price1){
         setID(ID);
         setName(name);
-        setPrice1(price1);
+        /*setPrice1(price1);
         setPrice2(price2);
-        setPrice3(price3);
+        setPrice3(price3);*/
         setMovieDescription(movieDescription);
         setMovieRating(movieRating);
 
@@ -29,7 +27,7 @@ public class Movie {
     public void setName(String name){
         this.name = name;
     }
-
+    /*
     public void setPrice1(double price1){
         this.price1 = price1;
     }
@@ -41,7 +39,7 @@ public class Movie {
     public void setPrice3(double price3){
         this.price3 = price3;
     }
-
+     */
     public void setMovieDescription(String movieDescription){
         this.movieDescription = movieDescription;
     }
@@ -49,4 +47,26 @@ public class Movie {
     public void setMovieRating(int movieRating){
         this.movieRating = movieRating;
     }
+
+    public int getID(){
+        return ID;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getMovieDescription(){
+        return movieDescription;
+    }
+
+    public int getMovieRating(){
+         return movieRating;
+    }
+
+    public String toString(){
+        return ID + " " + name + " " + movieDescription + " " + movieRating;
+
+    }
+
 }
